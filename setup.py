e101bdf4fd75a2bb2cd7be253ae983ae8592d29f
Joris Remmers <J.J.C.Remmers@tue.nl>
2020-04-27 12:26:28 +0200

    Adding setup to create a distribution

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
 
 setuptools.setup(
-    name="PyFEM",
-    version="0.0.1",
+    name="PyFEM-TUE",
+    version="3.0.2",
     author="J.J.C. Remmers",
-    author_email="author@example.com",
-    description="A python FEM package",
-    long_description=long_description,
+    author_email="j.j.c.remmers@tue.nl",
+    description="A Python Finite Element package",
+    keywords = ['Finite Element Method', 'Mechanics', 'Wiley'],
+    long_description="PyFEM is a python-based finite element code accompanies the book: 'Non-Linear Finite Element Analysis of Solids and Structures' by R. de Borst, M.A. Crisfield, J.J.C. Remmers and C.V. Verhoosel. The code is open source and intended for educational and scientific purposes only. If you use PyFEM in your research, the  developers would be grateful if you could cite the book in your work.",
     long_description_content_type="text/markdown",
     url="https://github.com/jjcremmers/PyFEM",
     packages=setuptools.find_packages(),
+    install_requires=['numpy','scipy','matplotlib'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
