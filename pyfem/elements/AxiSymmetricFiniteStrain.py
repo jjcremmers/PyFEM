b0ebcbfe79c2a29acecb305f57f269fce93aebd9
Joris Remmers <J.J.C.Remmers@tue.nl>
2020-06-25 16:44:22 +0200

    Adding AxiSymmetric Elements

---
+++ pyfem/elements/AxiSymmetricFiniteStrain.py
@@ -0,0 +1,245 @@
+############################################################################
+#  This Python file is part of PyFEM, the code that accompanies the book:  #
+#                                                                          #
+#    'Non-Linear Finite Element Analysis of Solids and Structures'         #
+#    R. de Borst, M.A. Crisfield, J.J.C. Remmers and C.V. Verhoosel        #
+#    John Wiley and Sons, 2012, ISBN 978-0470666449                        #
+#                                                                          #
+#  The code is written by J.J.C. Remmers, C.V. Verhoosel and R. de Borst.  #
+#                                                                          #
+#  The latest stable version can be downloaded from the web-site:          #
+#     http://www.wiley.com/go/deborst                                      #
+#                                                                          #
+#  A github repository, with the most up to date version of the code,      #
+#  can be found here:                                                      #
+#     https://github.com/jjcremmers/PyFEM                                  #
+#                                                                          #
+#  The code is open source and intended for educational and scientific     #
+#  purposes only. If you use PyFEM in your research, the developers would  #
+#  be grateful if you could cite the book.                                 #  
+#                                                                          #
+#  Disclaimer:                                                             #
+#  The authors reserve all rights but do not guarantee that the code is    #
+#  free from errors. Furthermore, the authors shall not be liable in any   #
+#  event caused by the use of the program.                                 #
+############################################################################
+
+from .Element import Element
+from pyfem.util.shapeFunctions  import getElemShapeData
+from pyfem.util.kinematics      import Kinematics
+
+from numpy import zeros, dot, outer, ones, eye, sqrt
+from math  import pi
+
+#------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
+
+class AxiSymmetricFiniteStrain( Element ):
+  
+  def __init__ ( self, elnodes , props ):
+    Element.__init__( self, elnodes , props )
+
+    if props.rank != 2:
+      print("Error")
+      
+    self.dofTypes = [ 'u' , 'v' ]
+         
+  def __type__ ( self ):
+    return name
+
+#------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
+
+  def getTangentStiffness ( self, elemdat ):
+
+    n = self.dofCount()
+
+    sData = getElemShapeData( elemdat.coords )
+   
+    for iData in sData:
+
+      r      = dot( elemdat.coords[:,0] , iData.h )
+      weight = 2.0*pi*r*iData.weight
+      
+      kin = self.getKinematics( iData.dhdx , iData.h , elemdat , r ) 
+      B   = self.getBmatrix   ( iData.dhdx , iData.h , kin.F , r )
+      
+      sigma,tang = self.mat.getStress( kin )
+      
+      t4 = self.tang6to4  ( tang  )
+      s4 = self.stress6to4( sigma )
+        
+      elemdat.stiff += dot ( B.transpose() , dot ( t4 , B ) ) * weight
+
+      geomstiff = zeros(shape=(n,n))
+      
+      for i,dpi in enumerate(iData.dhdx):
+        for j,dpj in enumerate(iData.dhdx):
+          geomstiff[2*i,2*j] += s4[0]*dpi[0]*dpj[0] + s4[1]*dpi[1]*dpj[1]
+          geomstiff[2*i,2*j] += s4[2]*iData.h[i]*iData.h[j]/(r*r)                 
+          geomstiff[2*i,2*j] += s4[3]*(dpi[0]*dpj[1] + dpi[1]*dpj[0])
+          
+          geomstiff[2*i+1,2*j+1] += s4[0]*dpi[0]*dpj[0] + s4[1]*dpi[1]*dpj[1]      
+          geomstiff[2*i+1,2*j+1] += s4[3]*(dpi[0]*dpj[1] + dpi[1]*dpj[0])
+          
+      elemdat.stiff += geomstiff * weight
+     
+      elemdat.fint  += dot ( B.transpose() , s4 ) * weight
+      
+      self.appendNodalOutput( self.mat.outLabels() , self.mat.outData() )
+
+#------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
+
+  def getInternalForce ( self, elemdat ):
+   
+    n = self.dofCount()
+   
+    sData = getElemShapeData( elemdat.coords )
+
+    for iData in sData:
+    
+      r      = dot( elemdat.coords[:,0] , iData.h )
+      weight = 2.0*pi*r*iData.weight
+      
+      kin = self.getKinematics( iData.dhdx , iData.h , elemdat , r ) 
+      B   = self.getBmatrix   ( iData.dhdx , iData.h , kin.F , r )
+                  
+      sigma,tang = self.mat.getStress( kin )
+       
+      s4 = self.stress6to4( sigma )
+      
+      elemdat.fint    += dot ( B.transpose() , s4 ) * weight
+
+      self.appendNodalOutput( self.mat.outLabels() , self.mat.outData() ) 
+
+#------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
+
+  def getMassMatrix ( self, elemdat ):
+      
+    sData = getElemShapeData( elemdat.coords )
+
+    rho = elemdat.matprops.rho
+
+    for iData in sData:
+    
+      r      = dot( elemdat.coords[:,0] , iData.h )
+      weight = 2.0*pi*r*iData.weight
+      
+      N  = self.getNmatrix( iData.h )
+      elemdat.mass += dot ( N.transpose() , N ) * rho * weight
+     
+    elemdat.lumped = sum(elemdat.mass)
+            
+#------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
+
+  def getKinematics( self , dphi , h , elemdat , r ):
+  
+    kin = Kinematics(3,6)
+
+    kin.F  = eye(3)
+    kin.F0 = eye(3)
+    
+    elstate  = elemdat.state
+    elstate0 = elstate - elemdat.Dstate
+    
+    invr = 1.0/r
+  
+    for i,(dp,p) in enumerate(zip(dphi,h)):
+      kin.F[0,0] += dp[0]*elstate[2*i]
+      kin.F[0,1] += dp[1]*elstate[2*i]
+      kin.F[1,0] += dp[0]*elstate[2*i+1]
+      kin.F[1,1] += dp[1]*elstate[2*i+1]
+      kin.F[2,2] += p*elstate[2*i] * invr
+      
+      kin.F0[0,0] += dp[0]*elstate0[2*i]
+      kin.F0[0,1] += dp[1]*elstate0[2*i]
+      kin.F0[1,0] += dp[0]*elstate0[2*i+1]
+      kin.F0[1,1] += dp[1]*elstate0[2*i+1]
+      kin.F0[2,2] += p*elstate0[2*i] * invr
+      
+    kin.E  = 0.5*(dot(kin.F.transpose(),kin.F)-eye(3))
+    kin.E0 = 0.5*(dot(kin.F0.transpose(),kin.F0)-eye(3))
+
+    dE = kin.E - kin.E0
+    
+    kin.strain[0] =     kin.E[0,0]
+    kin.strain[1] =     kin.E[1,1]
+    kin.strain[2] =     kin.E[2,2]
+    kin.strain[3] = 2.0*kin.E[1,2]
+    kin.strain[4] = 2.0*kin.E[0,2]
+    kin.strain[5] = 2.0*kin.E[0,1]
+    
+    return kin
+
+#------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
+
+  def getBmatrix( self , dphi , phi , F , r ):
+
+    B = zeros( shape=(4, 2*len(dphi) ) )
+
+    invr = 1.0/r
+    
+    for i,(dp,p) in enumerate(zip(dphi,phi)):
+      B[0,2*i  ] = dp[0]*F[0,0]
+      B[0,2*i+1] = dp[0]*F[1,0]
+	
+      B[1,2*i  ] = dp[1]*F[0,1]
+      B[1,2*i+1] = dp[1]*F[1,1]
+      
+      B[2,2*i  ] = p * F[2,2] * invr
+      B[2,2*i+1] = 0.0
+
+      B[3,2*i  ] = dp[1]*F[0,0]+dp[0]*F[0,1]
+      B[3,2*i+1] = dp[0]*F[1,1]+dp[1]*F[1,0]
+ 
+    return B
+
+#------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
+
+  def getNmatrix( self , h ):
+
+    N = zeros( shape=( 2 , 2*len(h) ) )
+
+    for i,a in enumerate( h ):
+      for j in range(2):
+        N[j,2*i+j] = a
+    
+    return N
+    
+    
+  def stress6to4( self , sigma ):
+  
+    s4 = zeros(4)
+    
+    s4[0] = sigma[0]
+    s4[1] = sigma[1]
+    s4[2] = sigma[2]
+    s4[3] = sigma[5]
+    
+    return s4
+    
+  def tang6to4( self , tang ):
+  
+    t4 = zeros( shape=(4,4) )
+    
+    t4[:3,:3] = tang[:3,:3]
+    t4[:3,3 ] = tang[:3,5]
+    t4[3,:3]  = tang[5,:3]
+    t4[3,3]   = tang[5,5]
+    
+    return t4
+    
+    
+    
---
+++ pyfem/elements/AxiSymmetricSmallStrain.py
@@ -0,0 +1,192 @@
+############################################################################
+#  This Python file is part of PyFEM, the code that accompanies the book:  #
+#                                                                          #
+#    'Non-Linear Finite Element Analysis of Solids and Structures'         #
+#    R. de Borst, M.A. Crisfield, J.J.C. Remmers and C.V. Verhoosel        #
+#    John Wiley and Sons, 2012, ISBN 978-0470666449                        #
+#                                                                          #
+#  The code is written by J.J.C. Remmers, C.V. Verhoosel and R. de Borst.  #
+#                                                                          #
+#  The latest stable version can be downloaded from the web-site:          #
+#     http://www.wiley.com/go/deborst                                      #
+#                                                                          #
+#  A github repository, with the most up to date version of the code,      #
+#  can be found here:                                                      #
+#     https://github.com/jjcremmers/PyFEM                                  #
+#                                                                          #
+#  The code is open source and intended for educational and scientific     #
+#  purposes only. If you use PyFEM in your research, the developers would  #
+#  be grateful if you could cite the book.                                 #  
+#                                                                          #
+#  Disclaimer:                                                             #
+#  The authors reserve all rights but do not guarantee that the code is    #
+#  free from errors. Furthermore, the authors shall not be liable in any   #
+#  event caused by the use of the program.                                 #
+############################################################################
+
+from .Element import Element
+from pyfem.util.shapeFunctions  import getElemShapeData
+from pyfem.util.kinematics      import Kinematics
+from numpy import zeros, dot, outer, ones , eye
+from math  import pi
+
+class AxiSymmetricSmallStrain( Element ):
+  
+  def __init__ ( self, elnodes , props ):
+  
+    Element.__init__( self, elnodes , props )
+
+    if props.rank != 2:
+      print("Error")
+    
+    self.dofTypes = [ 'u' , 'v' ]
+   
+    self.kin = Kinematics(3,6)
+
+  def __type__ ( self ):
+    return name
+
+#------------------------------------------------------------------------
+
+  def getTangentStiffness ( self, elemdat ):
+
+    sData = getElemShapeData( elemdat.coords )
+
+    for iData in sData:
+
+      r      = dot( elemdat.coords[:,0] , iData.h )
+      weight = 2.0*pi*r*iData.weight
+             
+      b = self.getBmatrix( iData.dhdx , iData.h , r )
+      
+      strain  = dot ( b , elemdat.state )
+      #self.kin.dstrain = dot ( b , elemdat.Dstate )
+      
+      self.kin.strain[0] = strain[0]
+      self.kin.strain[1] = strain[1]
+      self.kin.strain[2] = strain[2]
+      self.kin.strain[3] = 0.
+      self.kin.strain[4] = 0.
+      self.kin.strain[5] = strain[3]
+      
+      #self.kin.dstrain = dot ( b , elemdat.Dstate )
+      
+      sigma,tang = self.mat.getStress( self.kin )
+      
+      s4 = self.stress6to4( sigma )
+      t4 = self.tang6to4  ( tang )
+
+      elemdat.stiff += dot ( b.transpose() , dot ( t4 , b ) ) * weight
+      elemdat.fint  += dot ( b.transpose() , s4 ) * weight
+
+      self.appendNodalOutput( self.mat.outLabels() , self.mat.outData() )
+      
+    
+#-------------------------------------------------------------------------
+
+  def getInternalForce ( self, elemdat ):
+      
+    sData = getElemShapeData( elemdat.coords )
+
+    for iData in sData:
+    
+      r      = dot( elemdat.coords[:,0] , iData.h )
+      weight = 2.0*pi*r*iData.weight
+      
+      b = self.getBmatrix( iData.dhdx , iData.h , r )
+
+      strain  = dot ( b , elemdat.state )
+      #self.kin.dstrain = dot ( b , elemdat.Dstate )
+      
+      self.kin.strain[0] = strain[0]
+      self.kin.strain[1] = strain[1]
+      self.kin.strain[2] = strain[2]
+      self.kin.strain[3] = 0.
+      self.kin.strain[4] = 0.
+      self.kin.strain[5] = strain[3]
+      
+      #self.kin.dstrain = dot ( b , elemdat.Dstate )
+
+      sigma,tang = self.mat.getStress( self.kin )
+      
+      s4 = self.stress6to4( sigma )
+
+      elemdat.fint    += dot ( b.transpose() , s4 ) * weight
+
+      self.appendNodalOutput( self.mat.outLabels() , self.mat.outData() )
+      
+#----------------------------------------------------------------------
+    
+  def getMassMatrix ( self, elemdat ):
+      
+    sData = getElemShapeData( elemdat.coords )
+
+    rho = elemdat.matprops.rho
+
+    for iData in sData:
+    
+      r      = dot( elemdat.coords[0,:] , iData.h )
+      weight = 2.0*pi*r*iData.weight
+      
+      N  = self.getNmatrix( iData.h )
+      elemdat.mass += dot ( N.transpose() , N ) * rho * weight
+     
+    elemdat.lumped = sum(elemdat.mass)
+   
+#--------------------------------------------------------------------------
+
+  def getBmatrix( self , dphi , phi , r ):
+
+    b = zeros( shape=( 4 , self.dofCount() ) )
+
+    invr = 1.0/r
+    
+    for i,(dp,p) in enumerate(zip(dphi,phi)):
+      b[0,i*2+0] = dp[0]
+      b[1,i*2+1] = dp[1]
+      b[2,i*2+0] = p*invr
+      b[3,i*2+0] = dp[1]
+      b[3,i*2+1] = dp[0]
+ 
+    return b
+
+#------------------------------------------------------------------------------
+
+  def getNmatrix( self , h ):
+
+    N = zeros( shape=( 2 , 2*len(h) ) )
+
+    for i,a in enumerate( h ):
+      for j in list(range(2)):
+        N[j,2*i+j] = a
+    
+    return N
+    
+#
+
+  def stress6to4( self , sigma ):
+  
+    s4 = zeros(4)
+    
+    s4[0] = sigma[0]
+    s4[1] = sigma[1]
+    s4[2] = sigma[2]
+    s4[3] = sigma[5]
+    
+    return s4
+    
+  def tang6to4( self , tang ):
+  
+    t4 = zeros( shape=(4,4) )
+    
+    t4[:3,:3] = tang[:3,:3]
+    t4[:3,3 ] = tang[:3,5]
+    t4[3,:3]  = tang[5,:3]
+    t4[3,3]   = tang[5,5]
+    
+    return t4
+    
+    
+    
+    
+  
