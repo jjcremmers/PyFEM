b9419b20b6a55b9f624831e6969f882fac912e6d
Joris Remmers <J.J.C.Remmers@tue.nl>
2024-08-01 12:42:40 +0200

    Final version single panel

--- pyfem_gui.py
+++ pyfem_gui.py
@@ -1,5 +1,5 @@
 import sys
-import subprocess
+#import subprocess
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QTextEdit, QFileDialog, QVBoxLayout, QWidget, QToolBar, QMessageBox, QStyle
 from PySide6.QtCore import QProcess, Qt, QThread, Signal, QObject
 from PySide6.QtGui import QIcon, QAction, QKeySequence
@@ -7,6 +7,10 @@ from PySide6.QtGui import QIcon, QAction, QKeySequence
 from pyfem.io.InputReader   import InputRead
 from pyfem.io.OutputManager import OutputManager
 from pyfem.solvers.Solver   import Solver
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
 
 class EmittingStream(QObject):
     text_written = Signal(str)
@@ -17,7 +21,10 @@ class EmittingStream(QObject):
     def flush(self):
         pass 
         
-# Worker thread class
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+
 class WorkerThread(QThread):
     output_signal = Signal(str)
 
@@ -26,9 +33,6 @@ class WorkerThread(QThread):
         self.input_file = input_file
 
     def run(self):
-
-        #sys.stdout = EmittingStream(text_written=self.output_signal.emit)
-        #sys.stderr = EmittingStream(text_written=self.output_signal.emit)
         
         self.props,self.globdat = InputRead( self.input_file )
         
@@ -40,9 +44,10 @@ class WorkerThread(QThread):
             self.output.run( self.props , self.globdat )
 
         self.globdat.close()        
-
-        #sys.stdout = sys.__stdout__
-        #sys.stderr = sys.__stderr__
+  
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
                 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -73,6 +78,10 @@ class MainWindow(QMainWindow):
         self.emitting_stream.text_written.connect(self.handle_output)
         sys.stdout = self.emitting_stream
         sys.stderr = self.emitting_stream        
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
 
     def create_menu(self):
         menu_bar = self.menuBar()
@@ -115,29 +124,42 @@ class MainWindow(QMainWindow):
         about_action.triggered.connect(self.show_about)
         about_menu.addAction(about_action)
 
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+
     def create_toolbar(self):
         toolbar = QToolBar("Main Toolbar")
         self.addToolBar(toolbar)
 
-        new_action = QAction(self.style().standardIcon(QStyle.SP_FileIcon), 'New', self)        
+        new_action = QAction(self.style().standardIcon(QStyle.SP_FileIcon), 
+                     'New', self)        
         new_action.triggered.connect(self.load_file)
         toolbar.addAction(new_action)
 
-        open_action = QAction(self.style().standardIcon(QStyle.SP_DialogOpenButton), 'Open', self)        
+        open_action = QAction(self.style().standardIcon(QStyle.SP_DialogOpenButton),
+                      'Open', self)        
         open_action.triggered.connect(self.load_file)
         toolbar.addAction(open_action)
 
-        save_action = QAction(self.style().standardIcon(QStyle.SP_DialogSaveButton), "Save", self)
+        save_action = QAction(self.style().standardIcon(QStyle.SP_DialogSaveButton),
+                      "Save", self)
         save_action.triggered.connect(self.execute_script)
         toolbar.addAction(save_action)        
 
-        execute_action = QAction(self.style().standardIcon(QStyle.SP_MediaPlay), "Execute", self)        
+        execute_action = QAction(self.style().standardIcon(QStyle.SP_MediaPlay),
+                         "Execute", self)        
         execute_action.triggered.connect(self.execute_script)
         toolbar.addAction(execute_action)
 
-        abort_action = QAction(self.style().standardIcon(QStyle.SP_BrowserStop), "Abort", self)
+        abort_action = QAction(self.style().standardIcon(QStyle.SP_BrowserStop),
+                       "Abort", self)
         abort_action.triggered.connect(self.execute_script)
         toolbar.addAction(abort_action)
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
 
     def load_file(self):
         file_dialog = QFileDialog()
@@ -148,32 +170,45 @@ class MainWindow(QMainWindow):
         if self.input_file:
             self.output_terminal.append(f"Loaded file: {self.input_file}")
 
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+
     def execute_script(self):
         if not self.input_file:
             self.output_terminal.append("Please load an input file first.")
             return
-
-        #self.output_terminal.append("Executing script...")
-        #self.process.start("python", ["PyFEM.py", self.input_file])
         
         self.worker = WorkerThread(self.input_file)
         self.worker.output_signal.connect(self.handle_output)
         self.worker.start()   
         
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+        
     def handle_output(self, text):
         self.output_terminal.append( text.rstrip() )
-        #self.output_terminal.append(result)             
-
+      
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+           
     def update_output(self):
         output = self.process.readAllStandardOutput().data().decode()
         error = self.process.readAllStandardError().data().decode()
         self.output_terminal.append(output)
-        #self.output_terminal.append(error)        
-        #if error:
-        #    self.output_terminal.append(f"Error: {error}")
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
 
     def show_about(self):
-        QMessageBox.about(self, "About", "Python Script Executor\n\nDeveloped with PySide6.")
+        QMessageBox.about(self, "About", "PyFEM gui\n\nDeveloped with PySide6.")
+        
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
