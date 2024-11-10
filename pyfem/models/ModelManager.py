64917c9e05e3036281a385410de3c19dbd3314ae
Joris Remmers <J.J.C.Remmers@tue.nl>
2024-11-10 20:59:38 +0100

    Adding models

---
+++ pyfem/models/ModelManager.py
@@ -0,0 +1,54 @@
+################################################################################
+#  This Python file is part of PyFEM, the code that accompanies the book:      #
+#                                                                              #
+#    'Non-Linear Finite Element Analysis of Solids and Structures'             #
+#    R. de Borst, M.A. Crisfield, J.J.C. Remmers and C.V. Verhoosel            #
+#    John Wiley and Sons, 2012, ISBN 978-0470666449                            #
+#                                                                              #
+#  Copyright (C) 2011-2024. The code is written in 2011-2012 by                #
+#  Joris J.C. Remmers, Clemens V. Verhoosel and Rene de Borst and since        #
+#  then augmented and maintained by Joris J.C. Remmers.                        #
+#  All rights reserved.                                                        #
+#                                                                              #
+#  A github repository, with the most up to date version of the code,          #
+#  can be found here:                                                          #
+#     https://github.com/jjcremmers/PyFEM/                                     #
+#     https://pyfem.readthedocs.io/                                            #	
+#                                                                              #
+#  The original code can be downloaded from the web-site:                      #
+#     http://www.wiley.com/go/deborst                                          #
+#                                                                              #
+#  The code is open source and intended for educational and scientific         #
+#  purposes only. If you use PyFEM in your research, the developers would      #
+#  be grateful if you could cite the book.                                     #    
+#                                                                              #
+#  Disclaimer:                                                                 #
+#  The authors reserve all rights but do not guarantee that the code is        #
+#  free from errors. Furthermore, the authors shall not be liable in any       #
+#  event caused by the use of the program.                                     #
+################################################################################
+
+class ModelManager:
+
+  def __init__( self , props ):
+
+    modelTypes = [ "Contact" , "RVE" ]
+    
+    self.models = []
+
+    for modelType in modelTypes:
+   
+      #print("model",modelType,props)
+      if hasattr( props , modelType):
+            
+        modelProps = getattr( props, modelType )
+        modelProps.modelType = modelType
+       
+        exec("from pyfem.models."+modelType+" import "+modelType)       
+     
+        self.models.append( eval(modelType+"( props )") )
+       
+  def run( self , props , globdat ):
+
+    for model in self.models:
+      model.run( props , globdat )      
