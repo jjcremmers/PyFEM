eee8dac55ed12f946b489a5437e0c25218631e48
Joris Remmers <j.j.c.remmers@tue.nl>
2025-10-27 14:10:41 +0100

    Adding runAll

--- pyfem/__init__.py
+++ pyfem/__init__.py
@@ -1,7 +1,8 @@
 from .api import run
 from .fem.NodeSet import NodeSet
 from .fem.ElementSet import ElementSet
+from .core.executables import runAll
 
-__all__ = ["run","NodeSet","ElementSet"]
+__all__ = ["run","NodeSet","ElementSet","runAll"]
 __version__ = "0.1.0"
 
